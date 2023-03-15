2e616d5a175fb0bdc4c9457faaf49f1df1a72f32
wood.wu <kilokingw@gmail.com>
2023-03-15 23:26:17 +0800

    Updated upgrade react ver

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { Chat } from "react-tmi";
 function Home() {
+  const [messages, setMessages] = useState([]);
+  const channel = "dada6621";
   return (
     <div>
-      123
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      <Chat
+        channel={channel}
+        onMessage={(channel, userstate, message, self) => {
+          // 在這裡處理收到的聊天室訊息
+          setMessages((prevMessages) => [...prevMessages, message]);
+        }}
+      />
     </div>
   )
 }
